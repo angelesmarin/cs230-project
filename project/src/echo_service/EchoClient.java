47359b393b115684f99dceca3575c9b1a89b0532
angelesmarin <angeles.marinbatana@bsu.edu>
2024-09-12 13:38:12 -0400

    fixed code

--- project/src/echo_service/EchoClient.java
+++ project/src/echo_service/EchoClient.java
@@ -2,15 +2,12 @@ package echo_service;
 
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.nio.ByteBuffer;
 import java.time.Instant;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
-import java.util.Scanner;
 
 public class EchoClient {
     public static void main(String[] args) throws Exception {
@@ -18,22 +15,23 @@ public class EchoClient {
             System.out.println("Please specify <serverIP> and <serverPort>");
             return;
         }
+
         InetAddress serverIP = InetAddress.getByName(args[0]);
         int serverPort = Integer.parseInt(args[1]);
 
-        DatagramSocket socket = new DatagramSocket(); //new socket
-
+        DatagramSocket socket = new DatagramSocket();
         DatagramPacket request = new DatagramPacket(new byte[0], 0, serverIP, serverPort);
-        socket.send(request); //send empty packet
+        socket.send(request);
 
-        DatagramPacket reply = new DatagramPacket(new byte[4], 4); //server sends 4 byte int
+        DatagramPacket reply = new DatagramPacket(new byte[4], 4);
         socket.receive(reply);
-        socket.close(); //gets ready to receive empty packet
+        socket.close();
 
-        ByteBuffer buffer = ByteBuffer.wrap(reply.getData());
-        long secondsSince1900 = buffer.getInt() & 0xFFFFFFFFL;
+        byte[] serverReplyBytes = reply.getData();
 
-        long secondsSince1970 = secondsSince1900 - 2208988800L;
+        int intValue = ByteBuffer.wrap(serverReplyBytes).getInt();
+        long unsignedValue = Integer.toUnsignedLong(intValue);
+        long secondsSince1970 = unsignedValue - 2208988800L;
 
         Instant time = Instant.ofEpochSecond(secondsSince1970);
         ZonedDateTime dateTime = time.atZone(ZoneId.of("America/New_York"));
